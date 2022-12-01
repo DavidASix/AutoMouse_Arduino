14bab1e88fdd8ddc4b0f99d89d698d02ce9f5806
davidasix <davidandersonsix@gmail.com>
2022-12-01 00:06:12 -0500

    Wrote main function with weighted random chance of each event occuring.

--- src/main.cpp
+++ src/main.cpp
@@ -47,19 +47,24 @@ void click(int button) {
 void wiggle() {
   // Flashes LEDs to stimulate mouse sensor, simulating movement.
   int leds[]={rLed, gLed, bLed};
-  for (int i : leds) {
-    digitalWrite(i, LOW);
+  // Runs 20 * 3 * 100 ms (6seconds), lights flash 60 times each
+  for(int i = 0; i < 20; i++) {
+    for (int j : leds) {
+    digitalWrite(j, LOW);
     delay(50);
-    digitalWrite(i, HIGH);
+    digitalWrite(j, HIGH);
     delay(50);
+  }
   }
 }
 
 void scroll() {
-  // Mouse has special signal pin quadrature encoder. Not sure how to spoof.
-  // Tried hijacking the IRLED and the I/O pin, but couldn't get a phase that worked.
-  // Below procedure takes 5000ms seconds
-  for(int i = 0; i< 300; i++) {
+  // Mouse has special signal pin quadrature encoder.
+  // Below hijacks the single pin encoder and the IRLed giving it stimulous.
+  // Attempts to scroll for 6000ms (600 * 10). This is hit and miss if it works.
+  // If this was a proper 2 pin quadrature encoder I could control it percisely,
+  // but for my use case random works good enough!
+  for(int i = 0; i< 600; i++) {
       digitalWrite(ioPin, HIGH);
       digitalWrite(irLed, LOW);
       delay(2);
@@ -71,8 +76,19 @@ void scroll() {
 }
 
 void loop() {
-  digitalWrite(bLed, LOW);
-  scroll();
-  digitalWrite(bLed, HIGH);
-  delay(1000);
+  int selector = random(1, 101);
+  if (selector <= 20) {
+    // 20% Chance of click
+    click(random(1, 3) == 1 ? lmb : rmb);
+  } else if (selector <= 40) {
+    // 20% chance of scroll
+    // Blue LED is turned on as an indicator of action
+    digitalWrite(bLed, LOW);
+    scroll();
+    digitalWrite(bLed, HIGH);
+  } else {
+    // 50% chance of wiggle
+    wiggle();
+  }
+  delay(10000);
 }
