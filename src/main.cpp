fbe252e3c80cd98bb546d5c5efdfcb8def0f4bff
davidasix <davidandersonsix@gmail.com>
2022-11-30 20:12:31 -0500

    Set up variables, pin outputs, function declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <Arduino.h>
 
+// RGB Anodes
+int rLed = 2;
+int gLed = 3;
+int bLed = 4;
+
+// Right & Left mouse button
+int rmb = 7;
+int lmb = 8;
+
+// Quadrature Encoder Front & Back
+int qEf = 13;
+int qEb = 12;
+int qEfState = HIGH;
+int qEbState = LOW;
+
+void click();
+void wiggle();
+void scroll();
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(rLed, OUTPUT);
+  pinMode(gLed, OUTPUT);
+  pinMode(bLed, OUTPUT);
+  pinMode(rmb, OUTPUT);
+  pinMode(lmb, OUTPUT);
+  pinMode(qEf, OUTPUT);
+  pinMode(qEb, OUTPUT);
+  // Initial state of scroll
+  digitalWrite(qEf, qEfState);
+  digitalWrite(qEb, qEbState);
+  // Anodes set to high to have LEDs off.
+  digitalWrite(rLed, HIGH);
+  digitalWrite(gLed, HIGH);
+  digitalWrite(bLed, HIGH);
 }
 
 void loop() {
